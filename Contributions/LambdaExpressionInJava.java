a7fb47d0b499efc6956d312495bd05b27b3f968c
parikshitsm <mancharesp@gmail.com>
2021-10-02 23:06:06 +0530

    Demostrate basic HelloWorld program using Lambda Expressions in Java.

--- Contributions/LambdaExpressionInJava.java
+++ Contributions/LambdaExpressionInJava.java
@@ -5,9 +5,7 @@ public class LambdaExpressionInJava {
         HelloWorldFunctionInterface helloWorldFI = () -> System.out.println("Printing Hello world with Lambda expression !!");
         helloWorldFI.printHelloWorld();
     }
-
     private static void printHelloWorldWithoutLambda(){
         System.out.println("Printing Hello world without Lambda!!");
     }
-
 }
