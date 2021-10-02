28478d9c0361109827d903d310860667297925be
happy_heart_hacker <ayushsingh.bui@gmail.com>
2021-10-02 12:44:03 +0530

    update clock.py
    Updated clock.py

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ root=Tk()
 root.title('clock')
 
 def time():
-    string=strftime('%H:%M:%S ')
+    string=strftime('%H:%M:%S')
     label.config(text=string)
     label.after(1000,time)
 label= Label(root, font=('ds-digital',80), background="black",foreground='yellow') #install ds-digital font just searching on  google
