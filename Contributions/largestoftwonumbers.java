fd111f52297b00f0db9dd22b0e2f32dd8889a4a6
swrnv <swarnava199halder@gmail.com>
2021-10-02 17:36:03 +0530

    addd a new java snippet

---
+++ Contributions/largestoftwonumbers.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class largestof2{
+static void print2largest(int arr[], 
+                        int arr_size)
+{
+int i, first, second;
+if (arr_size < 2) 
+{
+    System.out.printf(" Invalid Input ");
+    return;
+}
+Arrays.sort(arr);
+for (i = arr_size - 2; i >= 0; i--) 
+{
+    if (arr[i] != arr[arr_size - 1]) 
+    {
+    System.out.printf("The second largest " + 
+                        "element is %d\n", arr[i]);
+    return;
+    }
+}
+
+System.out.printf("There is no second " +
+                    "largest element\n");
+}
+
+// Driver code
+public static void main(String[] args)
+{
+int arr[] = {12, 35, 1, 10, 34, 1};
+int n = arr.length;
+print2largest(arr, n);
+}
+}
