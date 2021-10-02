923cbd395e0e1d679e8949f335463642fabd10fd
SachinRaghuvanshi2003 <91306009+SachinRaghuvanshi2003@users.noreply.github.com>
2021-10-02 14:20:37 +0530

    Edited
    I have added comments and also corrected some print statements in order to make the program more user friendly and clear.

--- Triangle_Pattern.py
+++ Triangle_Pattern.py
@@ -1,11 +1,11 @@
-rows = int(input("Enter Triangle Rows = "))
+rows = int(input("Enter the number of rows = "))     #Taking input from user for the number of rows the program will run
 
-print("Triangle Pattern ")
+print("Triangular Pattern ")    
 
 for i in range(0, rows):
-    alphabet = 65
-    for j in range(rows, i, -1):
-        print(end = ' ')
-    for k in range(0, i + 1):
-        print('%c' %(alphabet + k), end = ' ')
-    print()
+    al= 65                                           #Using the ASCII Value of a for initiation.
+    for j in range(rows, i, -1):                     #For the number of elements in a row.
+        print(end = ' ')                             #In order to continue printing in the same line.
+    for k in range(0, i + 1):                        #For determing each element in a single row.
+        print('%c' %(al + k), end = ' ')
+    print()                                           #For moving to the next line after completion of a row.
