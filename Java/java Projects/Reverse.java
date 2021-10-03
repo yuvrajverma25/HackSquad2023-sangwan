725f52e2c26a0a0f8927c62931d386065c8d5fd3
Abhishek Nallana <60889353+abhinallana@users.noreply.github.com>
2021-10-03 19:52:42 +0530

    Updated different Condition of While

--- Java/java Projects/Reverse.java
+++ Java/java Projects/Reverse.java
@@ -2,13 +2,13 @@ package com.company;
 
 public class Reverse {
     public static void main(String[] args) {
-        int num = 56789;
-
-        int  ans = 0;
-        while (num>0){
+        int num = 56789, ans = 0;
+        
+        while (num != 0){
             int rem = num % 10;
-            num/= 10;
             ans = ans * 10 + rem;
+            num/= 10;
+            
         }
 
         System.out.println(ans);
