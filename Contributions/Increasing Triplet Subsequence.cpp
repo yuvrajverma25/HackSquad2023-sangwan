41ec9a469026b4f086a82517aee8cd602f57eb15
Ayushi Nanda <85018225+Ayushinanda@users.noreply.github.com>
2021-10-05 15:19:54 +0530

    Increasing Triplet Subsequence

---
+++ Contributions/Increasing Triplet Subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) 
+        int low = INT_MAX;
+        int mid = INT_MAX;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if(nums[i] <= low)
+               low = nums[i];
+             else if(nums[i] <= mid)
+                mid = nums[i];
+            else
+                return true;
+        
+        }
+            
+        return false;
+    }
+};
