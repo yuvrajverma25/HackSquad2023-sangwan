313c854532866fc3d6769c4becec2207fe5ed726
Arnav2982 <78140728+Arnav2982@users.noreply.github.com>
2021-10-07 09:11:50 +0530

    Update Binary Search.cpp

--- C++/Binary Search.cpp
+++ C++/Binary Search.cpp
@@ -1,44 +1,31 @@
-#include <iostream>
+#include<iostream>
+#include<cstdio>
+#include<cmath>
 using namespace std;
-
-// This program performs a binary search through an array, must be sorted to work
-int binarySearch(int array[], int size, int value) 
-{   
-    int first = 0,         // First array element       
-    last = size - 1,       // Last array element       
-    middle,                // Mid point of search       
-    position = -1;         // Position of search value   
-    bool found = false;        // Flag   
-    while (!found && first <= last) 
-    {      
-        middle = (first + last) / 2;     // Calculate mid point      
-        if (array[middle] == value)      // If value is found at mid      
-    	{         
-                found = true;         
-                position = middle;      
-        }      
-        else if (array[middle] > value)  // If value is in lower half         
-            last = middle - 1;      
-        else         
-            first = middle + 1;          // If value is in upper half   
-    }   
-    return position;
-}
-int main ()
+int binary_search(int A[], int l, int r, int key) 
 {
-    const int size = 5; // size initialization
-    int array[size] = {1, 2, 3, 4, 5}; // declare array of size 10
-    int value; // declare value to be searched for
-    int result; // declare variable that will be returned after binary search
+  int m;
+  while( l <= r )     
+  {
+    m = l + (r-l)/2;
+    if( A[m] == key )     // Element found
+      return m;
+    if( A[m] < key )         // Search in right part of list
+      l = m + 1;
+    else            // Search in left part of list
+      r = m - 1;
+  }
+  return -1;
+}
 
-    cout << "What value would you like to search for? "; // prompt user to enter value
-    cin >> value;
-    result = binarySearch(array, size, value);
-
-    if (result == -1) // if value isn't found display this message
-        cout << "Not found\n";
-    else  // If value is found, displays message
-        cout << "Your value is in the array.\n"; 
-  
-    return 0;
+int main() 
+{
+  int tag, x, array[]={10,11,12,13,14,25,26,37,48,59};
+  x = 26;        // element to be searched in the array
+  tag=binary_search(array,0,10,x);
+  if(tag != -1)
+    cout<<"Element found at location : "<<tag;
+  else
+    cout<<"Element not present in the array.";
+  return 0;
 }
