61cc69d571d321c0af6c71480156431d0c587511
Arnav2982 <78140728+Arnav2982@users.noreply.github.com>
2021-10-07 09:22:35 +0530

    Update sum of digits.cpp

--- C++/sum of digits.cpp
+++ C++/sum of digits.cpp
@@ -3,18 +3,18 @@
 using namespace std;
 
 // Function to check sum of digit using recursion
-int sum_of_digit(int n)
+int sum_of_digit(int a)
 {
-	if (n == 0)
+	if (a == 0)
 	return 0;
-	return (n % 10 + sum_of_digit(n / 10));
+	return (a % 10 + sum_of_ the_digit(a / 10));
 }
 
 // Driven Program to check above
 int main()
 {
 	int num = 12345;
-	int result = sum_of_digit(num);
-	printf("Sum of digits in %d is %d\n", num, result);
+	int result = sum_of_the_digit(num);
+	printf("Sum of digits in %d is %d\a", num, result);
 	return 0;
 }
