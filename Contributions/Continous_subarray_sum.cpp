73e02e58a2bc5ae4786ad18c975156eac64b8724
AAYUSHI KUNWAR <51057675+aayushi-kunwar13@users.noreply.github.com>
2021-10-11 13:14:10 +0530

    Added problem statement and explanation

--- Contributions/Continous_subarray_sum.cpp
+++ Contributions/Continous_subarray_sum.cpp
@@ -1,3 +1,7 @@
+//Problem Statement - To find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum. 
+//Time Complexity - O(n) 
+//Example - arr[] = {-2,-3,4,-1,-2,1,5,-3}   output = 7  ( Larget sum of contiguous subarray )
+
 #include<iostream>
 #include<limits.h>
 using namespace std;
@@ -9,11 +13,11 @@ int main(){
     int msf = INT_MIN;
     for(int i=0;i<8;i++){
         meh = meh + arr[i];
-
+        //if Max Ending Here (meh) is smaller than the arr[i] than start the max subarray from current element
         if(meh < arr[i]){
             meh = arr[i];
         }
-
+        //if Max Ending Here (meh) is greater than Max So Far (msf) than msf = meh 
         if(meh>msf){
             msf = meh;
         }
