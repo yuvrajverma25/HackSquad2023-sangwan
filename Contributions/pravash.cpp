e08b66ef88edcf1c0e6a7d67d4ad291c94b5d5dc
Pravash Purkayastha <purkayasthapravash123@gmail.com>
2021-10-11 11:18:51 +0530

    Special  cpp code snippet

---
+++ Contributions/pravash.cpp
@@ -0,0 +1,32 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+#define  iOS           ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define  ln            "\n"
+#define  all(x)        x.begin(), x.end()
+#define  rall(x)       x.end(), x.begin()
+#define  fill(a, b)    memset(a, b, sizeof(a))
+#define  cntns(c, x)   c.find(x) != c.end()
+#define  set_bits      __builtin_popcountll
+#define  getunique(v)  {sort(all(v)); v.erase(unique(all(v)), v.end());}
+
+#define  mod           1000000007
+#define  mod1          998244353
+#define  inf           1e18
+#define  pi            3.141592653589793238462
+
+using ll = long long; using ld = long double; using ull = unsigned long long;
+
+void fix(int d) { cout << fixed << setprecision(d); }
+
+void run_case() {
+
+}
+
+int main() { 
+	iOS
+    int tests; cin >> tests;
+    for(int tc = 0; tc < tests; tc++) {
+        run_case();
+    }
+}
