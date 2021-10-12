6425897fc44a16c40c6071ae24fcd4d4ea14e95f
akshat_12 <65230665+akki-1211@users.noreply.github.com>
2021-10-12 10:44:39 +0530

    Diameter_Of_Binary_Tree.py

--- Contributions/Armstrong.java
+++ Contributions/Armstrong.java
@@ -1,37 +1,47 @@
-import java.util.*;
-public class Armstrong{
-   public static void main(String[] args)
-   {									
-       int rem,que,rem1,que2,num;
-	   
-	   Scanner sc = new Scanner(System.in);
-	  
-	  num=sc.nextInt();
+import java.util.Scanner;
 
-	   rem=num%10;
-		que=num/10;
-	   rem1=que%10;
-	   que2=que/10;
-	   
-	   
-	   int arm  = (int) (Math.pow(rem,3)+Math.pow(rem1,3)+Math.pow(que2,3));
-	   
-	   
-	   if(arm==num){
-	   System.out.println(num +" is a Armstrong number");
-	   
-	   }
-	   
-	   else{
-	   System.out.println(num +" is not a Armstrong number");
-	   }
-	   
-	   
-	   
-	   
-	   
-	   
-   }
+class Armstrong_number
+
+{
+
+    public static void main(String[] args) 
+
+    {
+
+    
+
+    Scanner sc = new Scanner(System.in);
+
+    int n=sc.nextInt();
+
+    int r = 0;
+
+    int a = 0;
+
+    int s = 0;
+
+    r=n;
+
+    while(r!=0)  
+
+   {
+
+       a=r%10;
+
+       s=s+a*a*a;
+
+       r=r/10; 
+
+   } 
+
+   if(s==n)
+
+   System.out.println( n + " is a armstrong number");
+
+   else
+
+   System.out.println( n + " is not a armstrong number");
+
+    }
+
 }
-
-
