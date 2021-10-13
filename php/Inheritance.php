eca360dfa51efd1a223b29670b80bbd94254be3b
Kanagaraj <42266013+kanait84@users.noreply.github.com>
2021-10-13 11:05:12 +0400

    Create Inheritance.php

---
+++ php/Inheritance.php
@@ -0,0 +1,24 @@
+<?php
+/*This section explains about the Inheritance -  OOPS concept*/
+class Fruit {
+  public $name;
+  public $color;
+  public function __construct($name, $color) {
+    $this->name = $name;
+    $this->color = $color;
+  }
+  public function intro() {
+    echo "The fruit is {$this->name} and the color is {$this->color}.";
+  }
+}
+
+// Strawberry is inherited from Fruit
+class Strawberry extends Fruit {
+  public function message() {
+    echo "Am I a fruit or a berry? ";
+  }
+}
+$strawberry = new Strawberry("Strawberry", "red");
+$strawberry->message();
+$strawberry->intro();
+?>
