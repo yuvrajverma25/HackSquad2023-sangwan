079def4e791955853afd8a9955badf5c6da327da
Neha  Garg <50827585+neha0123@users.noreply.github.com>
2021-10-13 14:42:50 +0530

    Create Single number leetcode.cpp

---
+++ C++/Single number leetcode.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    
+    int singleNumber(vector<int>& nums) {
+        int exor=nums[0];
+        int n=nums.size();
+        for(int i=1;i<n;i++){
+            exor=exor^nums[i];
+        }
+        return exor;
+    }
+};
