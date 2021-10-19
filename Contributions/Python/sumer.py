92feb579795d0851042c7acce4653721c9f03797
tez-580 <92677016+tez-580@users.noreply.github.com>
2021-10-19 10:22:01 +0530

    Create sumer.py

---
+++ Contributions/Python/sumer.py
@@ -0,0 +1,5 @@
+number1 = input('Enter first number: ')
+number2 = input('Enter second number: ')
+number3 = input('Enter third number')
+sum = int(number1) + int(number2) + int(number3)
+print(sum)
