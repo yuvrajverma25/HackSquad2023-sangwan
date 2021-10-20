0b2c278b1bbd4dc3f3d5cbffa84941f5f7f67d96
Akshat Sood <91633748+akshatsood2024@users.noreply.github.com>
2021-10-21 00:40:50 +0530

    Added Sorting Algorithms in CPP

--- C++/TimSort.cpp
+++ C++/TimSort.cpp
@@ -1,42 +1,55 @@
-// C++ program to perform TimSort.
-#include<bits/stdc++.h>
+/*TimSort is a sorting algorithm based on Insertion Sort and Merge Sort.
+
+A stable sorting algorithm works in O(n Log n) time and O(n) in best case.
+Used in Java’s Arrays.sort() as well as Python’s sorted() and sort().
+First sort small pieces using Insertion Sort, then merges the pieces using merge of merge sort.
+We divide the Array into blocks known as Run. 
+We sort those runs using insertion sort one by one and then merge those runs using the combine function used in merge sort. 
+If the size of the Array is less than run, then Array gets sorted just by using Insertion Sort. 
+The size of the run may vary from 32 to 64 depending upon the size of the array. 
+Note that the merge function performs well when size subarrays are powers of 2. 
+The idea is based on the fact that insertion sort performs well for small arrays.
+
+*/
+//#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
 const int RUN = 32;
- 
+
 // This function sorts array from left index to
 // to right index which is of size atmost RUN
-void insertionSort(int arr[], int left, int right)
+void insertion_sort(int A[], int left, int right)
 {
     for (int i = left + 1; i <= right; i++)
     {
-        int temp = arr[i];
+        int temp = A[i];
         int j = i - 1;
-        while (j >= left && arr[j] > temp)
+        while (j >= left && A[j] > temp)
         {
-            arr[j+1] = arr[j];
+            A[j + 1] = A[j];
             j--;
         }
-        arr[j+1] = temp;
+        A[j + 1] = temp;
     }
 }
- 
+
 // Merge function merges the sorted runs
-void merge(int arr[], int l, int m, int r)
+void merge(int A[], int l, int m, int r)
 {
-     
+
     // Original array is broken in two parts
     // left and right array
     int len1 = m - l + 1, len2 = r - m;
     int left[len1], right[len2];
     for (int i = 0; i < len1; i++)
-        left[i] = arr[l + i];
+        left[i] = A[l + i];
     for (int i = 0; i < len2; i++)
-        right[i] = arr[m + 1 + i];
- 
+        right[i] = A[m + 1 + i];
+
     int i = 0;
     int j = 0;
     int k = l;
- 
+
     // After comparing, we
     // merge those two array
     // in larger sub array
@@ -44,100 +57,99 @@ void merge(int arr[], int l, int m, int r)
     {
         if (left[i] <= right[j])
         {
-            arr[k] = left[i];
+            A[k] = left[i];
             i++;
         }
         else
         {
-            arr[k] = right[j];
+            A[k] = right[j];
             j++;
         }
         k++;
     }
- 
+
     // Copy remaining elements of left, if any
     while (i < len1)
     {
-        arr[k] = left[i];
+        A[k] = left[i];
         k++;
         i++;
     }
- 
+
     // Copy remaining element of right, if any
     while (j < len2)
     {
-        arr[k] = right[j];
+        A[k] = right[j];
         k++;
         j++;
     }
 }
- 
+
 // Iterative Timsort function to sort the
 // array[0...n-1] (similar to merge sort)
-void timSort(int arr[], int n)
+void tim_sort(int A[], int n)
 {
-     
+
     // Sort individual subarrays of size RUN
-    for (int i = 0; i < n; i+=RUN)
-        insertionSort(arr, i, min((i+RUN-1),
-                                    (n-1)));
- 
+    for (int i = 0; i < n; i += RUN)
+        insertion_sort(A, i, min((i + RUN - 1), (n - 1)));
+
     // Start merging from size RUN (or 32).
     // It will merge
     // to form size 64, then 128, 256
     // and so on ....
     for (int size = RUN; size < n;
-                             size = 2*size)
+         size = 2 * size)
     {
-         
+
         // pick starting point of
         // left sub array. We
         // are going to merge
-        // arr[left..left+size-1]
-        // and arr[left+size, left+2*size-1]
+        // A[left..left+size-1]
+        // and A[left+size, left+2*size-1]
         // After every merge, we
         // increase left by 2*size
         for (int left = 0; left < n;
-                             left += 2*size)
+             left += 2 * size)
         {
-             
+
             // find ending point of
             // left sub array
             // mid+1 is starting point
             // of right sub array
             int mid = left + size - 1;
-            int right = min((left + 2*size - 1),
-                                            (n-1));
- 
-            // merge sub array arr[left.....mid] &
-            // arr[mid+1....right]
-              if(mid < right)
-                merge(arr, left, mid, right);
+            int right = min((left + 2 * size - 1),
+                            (n - 1));
+
+            // merge sub array A[left.....mid] &
+            // A[mid+1....right]
+            if (mid < right)
+                merge(A, left, mid, right);
         }
     }
 }
- 
+
 // Utility function to print the Array
-void printArray(int arr[], int n)
+void printArray(int A[], int n)
 {
     for (int i = 0; i < n; i++)
-        printf("%d  ", arr[i]);
+        printf("%d  ", A[i]);
     printf("\n");
 }
- 
+
 // Driver program to test above function
 int main()
 {
-    int arr[] = {-2, 7, 15, -14, 0, 15, 0, 7, -7,
-                       -4, -13, 5, 8, -14, 12};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printf("Given Array is\n");
-    printArray(arr, n);
- 
+    int A[] = {-2, 7, 15, -14, 0, 15, 0, 7, -7,
+                 -4, -13, 5, 8, -14, 12};
+    int n = sizeof(A) / sizeof(A[0]);
+    cout<<"Given Array is\n";
+    printArray(A, n);
+
     // Function Call
-    timSort(arr, n);
- 
-    printf("After Sorting Array is\n");
-    printArray(arr, n);
+    tim_sort(A, n);
+
+    cout<<"\nAfter Sorting Array is\n";
+    printArray(A, n);
     return 0;
 }
