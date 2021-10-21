2b160509fa37aae9f061ffb537611b715d2f53b1
Gifty <87938653+Giffff@users.noreply.github.com>
2021-10-21 22:54:20 +0530

    Adding inverted_numberpyramid.c

---
+++ inverted_numberpyramid.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int i,j,n;
+printf("enter no of rows needed\n");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+        for(j=1;j<=i;j++)
+        {
+        printf("%d",i); 
+        }
+printf("\n");
+}
+}
