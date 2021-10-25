b93484e81cf7b2d58e0c3855ff6b9c6925b4a9e8
Geethegreat <86944224+Geethegreat@users.noreply.github.com>
2021-10-25 10:23:50 +0530

    c++, DSF,

--- Python/binarySearch.py
+++ Python/binarySearch.py
@@ -1,38 +1,23 @@
-def binarySearch(x, u):
-	l = 0
-	h = len(x) - 1
-	while l <= h:
-		m = (h + l) // 2
-		# Right half
-		if x[m] < u:
-			l = m + 1
+def binarysearch(arr, element):
+    start = 0
+    end = len(arr) - 1
+    while start <= end:
+        mid = (start + end) // 2
+        if arr[mid] == element:
+            return mid
+        elif arr[mid] < element:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return -1
 
-		# Left half
-		elif x[m] > u:
-			h = m - 1
 
-		# Mid
-		else:
-			return m
-	# Not in list
-	return False
-
-x = list(map(int,input("Enter the list of numbers separated by space: ").split()))
-u = int(input("Enter the number you want to search: "))
-
-res = binarySearch(x, u)
-
-if res != False:
-	print("Number is present at:", str(res))
+faltu =int(input())
+if faltu == 0:
+    list = [0]
 else:
-	print("Number is not preset in list")
-
-
-'''
-Input:  x = 0 1 2 3 4 5 6 7 8 9 10
-	u = 8
-Output: Number is present at: 8
-
-Average Time complexity: O(logN)
-Space complexity: O(1)
-'''
+    list = [int(x) for x in input().split()]
+no_of_loop = int(input())
+for i in range(no_of_loop):
+    search_element = int(input())
+    print(binarysearch(list, search_element))
