6a8d03f8c605523b7889c7038243ba7b4d6cf5b7
Ravikant-git <90864334+Ravikant-git@users.noreply.github.com>
2021-10-26 12:10:51 +0530

    Create findlengthofstring.cpp

---
+++ Contributions/findlengthofstring.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  char const *s="welcome";
+  int i;
+  for(i=0;s[i]!='\0';i++)
+  {
+  }
+  cout<<"Length of string is: "<<i<<endl;
+
+ return 0;
+}
