9df664e18f7f12dfde44a89ffc03f7a8338aa048
ex [offline] <karthik.lal558@gmail.com>
2021-10-29 20:32:54 +0530

    For Hacktoberfest

---
+++ C++/Calculation-simple/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2,sum;
+
+    cout<<"Welcome to C++ Calculator"<<endl;
+    cout<<"";
+    cout<<"Enter Your First Number : ";cin>>n1;
+    cout<<"Enter Your Second Number : ";cin>>n2;
+    sum=n1+n2;
+
+    cout<<"Total is : "<<sum<<endl;
+    cout<<"";
+    cout<<"Thanks for using C++ Calculator";
+    return 0;
+}
