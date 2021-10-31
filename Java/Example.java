578c940031f24cafa96de4d4d4c5d8753be964a2
pragati gauraha <93464042+GaurahaP@users.noreply.github.com>
2021-10-31 12:37:06 +0530

    added example.java

---
+++ Java/Example.java
@@ -0,0 +1,10 @@
+/* This is a simple Java Program.
+Call this file Example.java.
+*/
+class Example{
+//A Java Program begins with a call to main().
+
+public static void main(String args[]){
+System.out.println("Java drives the Web.");
+}
+}
