d50f00e605980f84f4f34dd0c59eea3ffa4f4dee
Patrick Emmanuel Thanistas <thanithanis171198@gmail.com>
2022-10-16 21:57:39 +0530

    Add portfolio template

--- C++/Factorial.cpp
+++ C++/Factorial.cpp
@@ -13,8 +13,6 @@ int factorial(int number)
   	}
 }
 
-
-//driver function
 int main()
 {
   int number;
--- C++/Matrix.cpp
+++ C++/Matrix.cpp
@@ -1,157 +1,69 @@
-#include<iostream>
-using namespace std;
-template<class T>
-class matrix
+#include <iostream.h>
+#include<conio.h>
+int main()
 {
-	T ** matr;
-	int rows,colm;
-public:
-	matrix()
-	{
-		matr=nullptr;
-		rows=0;
-		colm=0;
-	}
-	matrix(int r,int c)
-	{
-		
-		rows=r;
-		colm=c;
-		matr=new T*[r];
-		for(int i=0;i<r;i++)
-		{
-			matr[i]=new T[c];
-		}
-	}
-	matrix(matrix & obj)
-	{
-		this->rows=obj.rows;
-		this->colm=obj.colm;
-		this->matr=new T*[obj.rows];
-		for(int i=0;i<obj.rows;i++)
-		{
-			matr[i]=new T[obj.colm];
-		}
-		for(int i=0;i<obj.rows;i++)
-		{
-			for(int j=0;j<obj.colm;j++)
-			{
-				this->matr[i][j]=obj.matr[i][j];
-			}
-		}
-	}
-	void insertmatrix(T const& ele,int r,int c)
-	{
-		this->matr[r][c]=ele;
-	}
-	matrix<T> operator + (matrix const &obj)
-	{
-		matrix mtemp;
-		mtemp.rows=this->rows;
-		mtemp.colm=this->colm;
-		mtemp.matr=new T*[obj.rows];
-		for(int i=0;i<obj.rows;i++)
-		{
-			mtemp.matr[i]=new T[obj.colm];
-		}
-		if(obj.colm==this->colm &&obj.rows==this->rows)
-		{
-			for(int i=0;i<obj.rows;i++)
-			{
-				for(int j=0;j<obj.colm;j++)
-				{
-					mtemp.matr[i][j]=this->matr[i][j]+obj.matr[i][j];
-				}
-			}
-		}
-		else
-		{
-			cout<<"mismatch occur\n";
-		}
-		return mtemp;
-	}
-	void print()
-	{
-		for(int i=0;i<this->rows;i++)
-		{
-			for(int j=0;j<this->colm;j++)
-			{
-				cout<<this->matr[i][j]<<'\t';
-			}
-			cout<<'\n';
-		}
-	}
-	void transpose()
-	{
-		matrix temp;
-		int r=this->colm;
-		int c=this->rows;
-		temp.matr=new T*[r];
-		for(int i=0;i<r;i++)
-		{
-			temp.matr[i]=new T[this->rows];
-		}
-		for(int i=0;i<this->colm;i++)
-		{
-			for(int j=0;j<this->rows;j++)
-			{
-				temp.matr[i][j]=this->matr[j][i];
-			}
-		}
-		for(int i=0;i<this->rows;i++)
-		{
-			delete[] matr[i];
-		}
-		delete [] matr;
-		this->rows=r;
-		this->colm=c;
-		this->matr=new T*[r];
-		for(int i=0;i<r;i++)
-		{
-			matr[i]=new T[c];
-		}
-		for(int i=0;i<r;i++)
-		{
-			for(int j=0;j<c;j++)
-			{
-				matr[i][j]=temp.matr[i][j];
-			}
-		}
-	}
-	~matrix()
-	{
-		if(matr!=nullptr)
-		{
-			for(int i=0;i<rows;i++)
-			{
-				if(matr[i]!=nullptr)
-				delete [] matr[i];
-			}
-			delete [] matr;
-		}
-	}
-};
-void main()
-{
-matrix<int> m1(2, 3);
-m1.insertmatrix(1, 0, 0);
-m1.insertmatrix(1, 0, 1);
-m1.insertmatrix(1, 0, 2);
-m1.insertmatrix(0, 1, 0);
-m1.insertmatrix(0, 1, 1);
-m1.insertmatrix(0, 1, 2);
-m1.transpose();
-matrix<int> m2(2, 3);
-m2.insertmatrix(-1, 0, 0);
-m2.insertmatrix(-1, 0, 1);
-m2.insertmatrix(-1, 0, 2);
-m2.insertmatrix(10, 1, 0);
-m2.insertmatrix(5, 1, 1);
-m2.insertmatrix(1, 1, 2);
-m2.transpose();
-matrix<int> m3(m2);
-matrix<int> m4(m1 + m3);
-m4.transpose();
-m4.print();
-system("pause");
+    int a[10][10], b[10][10], mult[10][10], r1, c1, r2, c2, i, j, k;
+
+    cout << "Enter rows and columns for first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns for second matrix: ";
+    cin >> r2 >> c2;
+
+    // If column of first matrix in not equal to row of second matrix,
+    // ask the user to enter the size of matrix again.
+    while (c1!=r2)
+    {
+        cout << "Error! column of first matrix not equal to row of second.";
+
+        cout << "Enter rows and columns for first matrix: ";
+        cin >> r1 >> c1;
+
+        cout << "Enter rows and columns for second matrix: ";
+        cin >> r2 >> c2;
+    }
+
+    // Storing elements of first matrix.
+    cout << endl << "Enter elements of matrix 1:" << endl;
+    for(i = 0; i < r1; ++i)
+        for(j = 0; j < c1; ++j)
+        {
+            cout << "Enter element a" << i + 1 << j + 1 << " : ";
+            cin >> a[i][j];
+        }
+
+    // Storing elements of second matrix.
+    cout << endl << "Enter elements of matrix 2:" << endl;
+    for(i = 0; i < r2; ++i)
+        for(j = 0; j < c2; ++j)
+        {
+            cout << "Enter element b" << i + 1 << j + 1 << " : ";
+            cin >> b[i][j];
+        }
+
+    // Initializing elements of matrix mult to 0.
+    for(i = 0; i < r1; ++i)
+        for(j = 0; j < c2; ++j)
+        {
+            mult[i][j]=0;
+        }
+
+    // Multiplying matrix a and b and storing in array mult.
+    for(i = 0; i < r1; ++i)
+        for(j = 0; j < c2; ++j)
+            for(k = 0; k < c1; ++k)
+            {
+                mult[i][j] += a[i][k] * b[k][j];
+            }
+
+    // Displaying the multiplication of two matrix.
+    cout << endl << "Output Matrix: " << endl;
+    for(i = 0; i < r1; ++i)
+    for(j = 0; j < c2; ++j)
+    {
+        cout << " " << mult[i][j];
+        if(j == c2-1)
+            cout << endl;
+    }
+
+    return 0;
 }
