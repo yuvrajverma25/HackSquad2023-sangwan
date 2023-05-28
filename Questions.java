34f8ffac1ee9f8ac98da03b547052e3237a06372
Prasenjit Nayak <92244026+StarKnightt@users.noreply.github.com>
2023-05-28 23:11:05 +0530

    Merge

---
+++ Questions.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ public static void main(String[] args) {
+        /*
+         * Qn --> 1 To sum three numbers 
+         */
+        int a = 43;
+        int b = 23;
+        int c = 45;
+        int sum = (a+b+c);
+        System.out.println(sum);
+        /*
+         * Qn --> 2 To calculate CGPA using marks of three subjects (out of 100)
+         */
+        float subject1 = 89.5f;
+        float Subject2 = 93.4f;
+        float subject3 = 23.5f;
+        float CGPA = (subject1+Subject2+subject3)/30;
+        System.out.println(CGPA);
+ }
+}
