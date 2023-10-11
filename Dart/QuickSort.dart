fc123a99c67e5c1a416743e2e208748184b7e0e5
Biswa Baibhab Subudhi <94667039+biswabaibhab007@users.noreply.github.com>
2023-10-11 23:02:27 +0530

    Create QuickSort.dart

---
+++ Dart/QuickSort.dart
@@ -0,0 +1,24 @@
+void quickSort(List<int> arr, int low, int high) {
+  if (low < high) {
+    int pivotIndex = partition(arr, low, high);
+    quickSort(arr, low, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, high);
+  }
+}
+
+int partition(List<int> arr, int low, int high) {
+  int pivot = arr[high];
+  int i = low - 1;
+  for (int j = low; j < high; j++) {
+    if (arr[j] < pivot) {
+      i++;
+      int temp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = temp;
+    }
+  }
+  int temp = arr[i + 1];
+  arr[i + 1] = arr[high];
+  arr[high] = temp;
+  return i + 1;
+}
