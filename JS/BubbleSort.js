1f393227a229e601db10f9ae87dc3ad912afd458
Biswa Baibhab Subudhi <94667039+biswabaibhab007@users.noreply.github.com>
2023-10-11 22:13:14 +0530

    Create BubbleSort.js

---
+++ JS/BubbleSort.js
@@ -0,0 +1,24 @@
+function bubbleSort(arr) {
+  const n = arr.length;
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap arr[i] and arr[i+1]
+        const temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+
+  return arr;
+}
+
+// Example usage:
+const arr = [64, 34, 25, 12, 22, 11, 90];
+const sortedArray = bubbleSort(arr);
+console.log("Sorted array: " + sortedArray);
