d8475b092fa8f76a307cd154ca44b86aa69e0586
Biswa Baibhab Subudhi <94667039+biswabaibhab007@users.noreply.github.com>
2023-10-11 22:59:37 +0530

    Create InsertionSort.dart

---
+++ Dart/InsertionSort.dart
@@ -0,0 +1,12 @@
+void insertionSort(List<int> arr) {
+  int n = arr.length;
+  for (int i = 1; i < n; i++) {
+    int current = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > current) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = current;
+  }
+}
