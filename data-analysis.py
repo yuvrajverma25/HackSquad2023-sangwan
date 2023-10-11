80f0a4458f6152bf7889c7874a75a894a84847a0
PRIYANSHU VARSHNEY <124420199+harshhere905@users.noreply.github.com>
2023-10-12 02:15:46 +0530

    Create data-analysis.py

---
+++ data-analysis.py
@@ -0,0 +1,8 @@
+import statistics
+
+data = [23, 45, 67, 12, 90, 34, 56]
+mean_value = statistics.mean(data)
+median_value = statistics.median(data)
+
+print("Mean:", mean_value)
+print("Median:", median_value)
