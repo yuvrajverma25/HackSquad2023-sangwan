de95ce73b5df31f099484b1b396229867110f7b9
Utkarsh Kumar <146640299+utkarsh1042005@users.noreply.github.com>
2023-10-12 09:54:53 +0530

    Create c_to_f.cpp

---
+++ c_to_f.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float c;
+    cout<<"Enter value in degree celcius"<<endl;
+    cin>>c;
+    float f=1.8*c+32;
+    cout<<"Value in fahrenheit is "<< f <<endl;
+
+}
