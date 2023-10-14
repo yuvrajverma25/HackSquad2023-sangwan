de6667fd0a3a78e74f7161f5eb1556fabcba08e2
Shukla0404 <147617524+Shukla0404@users.noreply.github.com>
2023-10-14 16:09:09 +0530

    Create SpiralMatrix.cpp

---
+++ C++/SpiralMatrix.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+class SpiralMatrix {
+public:
+    std::vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int left = 0, right = m - 1;
+        int top = 0, bottom = n - 1;
+        std::vector<int> result;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.push_back(matrix[top][i]);
+            }
+            top++;
+
+            for (int i = top; i <= bottom; i++) {
+                result.push_back(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    result.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    result.push_back(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    SpiralMatrix spiral;
+    std::vector<std::vector<int>> matrix = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    std::vector<int> result = spiral.spiralOrder(matrix);
+
+    for (int num : result) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
