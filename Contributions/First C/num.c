00291d860ef1b13a7ef19d44ccdb3694b18f5f94
Neonnnnnmnn <147932899+Neonnnnnmnn@users.noreply.github.com>
2023-10-14 22:18:30 +0530

    Create num.c

---
+++ Contributions/First C/num.c
@@ -0,0 +1,13 @@
+#include <stdio.h>      
+#include <conio.h>  
+#include <stdlib.h>  
+void main()  
+{     
+    // use rand() function to generate the number  
+    printf (" The random number is: %d", rand());  
+    printf ("\n The random number is: %d", rand());  
+      
+    printf (" \n The random number is: %d", rand());  
+    printf ("\n The random number is: %d", rand());  
+    getch();  
+}  
