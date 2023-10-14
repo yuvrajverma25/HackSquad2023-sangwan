ba9324f6723aa2d8e729fe5f38eae41d7dc2daf1
Mritul <84136875+mritul@users.noreply.github.com>
2023-10-14 22:51:07 +0530

    Add js code

---
+++ maximumSubarraySum.js
@@ -0,0 +1,14 @@
+function maxSubArraySum(arr) {
+    let maxEndingHere = arr[0];
+    let maxSoFar = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEndingHere = Math.max(arr[i], maxEndingHere + arr[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);
+    }
+
+    return maxSoFar;
+}
+
+const arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log("Maximum subarray sum:", maxSubArraySum(arr));
---
+++ maximumSubarraySum.kt
@@ -0,0 +1,16 @@
+fun maxSubArraySum(arr: IntArray): Int {
+    var maxEndingHere = arr[0]
+    var maxSoFar = arr[0]
+
+    for (i in 1 until arr.size) {
+        maxEndingHere = maxOf(arr[i], maxEndingHere + arr[i])
+        maxSoFar = maxOf(maxSoFar, maxEndingHere)
+    }
+
+    return maxSoFar
+}
+
+fun main() {
+    val arr = intArrayOf(-2, 1, -3, 4, -1, 2, 1, -5, 4)
+    println("Maximum subarray sum: ${maxSubArraySum(arr)}")
+}
