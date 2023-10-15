644b0e449b78e55f5f4e876f954fa0cc39152a28
SATYAM NAMDEV <111422209+Spyrosigma@users.noreply.github.com>
2023-10-16 00:10:21 +0530

    Added a basic calculator (With soothing CSS)

---
+++ calculator/script.js
@@ -0,0 +1,19 @@
+function clearDisplay() {
+  document.getElementById("display").value = "";
+}
+function deleteDisplay() {
+  document.getElementById("display").value = "";
+}
+
+function appendToDisplay(value) {
+  document.getElementById("display").value += value;
+}
+
+function calculateResult() {
+  const display = document.getElementById("display");
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
