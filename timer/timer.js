5b5b49e87d7128e8292f701c8c09602160cf8d01
Ayush Varshney <97428742+Avtech04@users.noreply.github.com>
2023-10-15 17:00:46 +0530

    Timer added

---
+++ timer/timer.js
@@ -0,0 +1,46 @@
+
+
+//clock updation in HTML
+async function flipAllCards(time) {
+
+    const seconds = time % 60
+    const minutes = Math.floor(time / 60) % 60
+    const hours = Math.floor(time / 3600)
+
+    flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10))
+    flip(document.querySelector("[data-hours-ones]"), hours % 10)
+    flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10))
+    flip(document.querySelector("[data-minutes-ones]"), minutes % 10)
+    flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10))
+    flip(document.querySelector("[data-seconds-ones]"), seconds % 10)
+}
+
+function flip(flipCard, newNumber) {
+    const topHalf = flipCard.querySelector(".top")
+    const startNumber = parseInt(topHalf.textContent)
+    if (newNumber === startNumber) return
+
+    const bottomHalf = flipCard.querySelector(".bottom")
+    const topFlip = document.createElement("div")
+    topFlip.classList.add("top-flip")
+    const bottomFlip = document.createElement("div")
+    bottomFlip.classList.add("bottom-flip")
+
+    top.textContent = startNumber
+    bottomHalf.textContent = startNumber
+    topFlip.textContent = startNumber
+    bottomFlip.textContent = newNumber
+
+    topFlip.addEventListener("animationstart", e => {
+        topHalf.textContent = newNumber
+    })
+    topFlip.addEventListener("animationend", e => {
+        topFlip.remove()
+    })
+    bottomFlip.addEventListener("animationend", e => {
+        bottomHalf.textContent = newNumber
+        bottomFlip.remove()
+    })
+    flipCard.append(topFlip, bottomFlip)
+}
+
