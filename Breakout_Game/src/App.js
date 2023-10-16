2fba585b7160e7827f7fbf467d057c66b62bb599
Shubham Shinde <123376721+shubham-shinde-442@users.noreply.github.com>
2023-10-16 20:08:47 +0530

    Breakout Added

---
+++ Breakout_Game/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Breakout from "./games/breakout";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+
+          <Breakout />
+          
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ Breakout_Game/src/data.js
@@ -0,0 +1,29 @@
+export default {
+  ballObj: {
+    x: 20,
+    y: 200,
+    dx: 5,
+    dy: 5,
+    rad: 10,
+    speed: 12,
+  },
+  brickObj: {
+    x: 0.5,
+    y: 50,
+    height: 20,
+    density: 2,
+    colors: ["red", "lightblue"],
+  },
+  player: {
+    name: "Rayy",
+    lives: 6,
+    score: 0,
+    level: 3,
+  },
+  paddleProps: {
+    height: 30,
+    width: 100,
+    x: 100,
+    color: "orange",
+  },
+};
