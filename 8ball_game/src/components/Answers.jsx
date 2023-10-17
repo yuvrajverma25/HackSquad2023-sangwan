62835d2c156df069278fdd142743a87fbdf351db
Shubham Shinde <123376721+shubham-shinde-442@users.noreply.github.com>
2023-10-17 21:02:10 +0530

    8Ball Game Added

---
+++ 8ball_game/src/components/Answers.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+
+const answers = [
+    {
+        id: 1,
+        answer: "What the fuck !?",
+    },
+    {
+        id: 2,
+        answer: "Tu peux compter là dessus !",
+    },
+    {
+        id: 3,
+        answer: "Demande plutôt à Lucas !",
+    },
+    {
+        id: 4,
+        answer: "Pourquoi pas, laisse moi y réfléchir.",
+    },
+    {
+        id: 5,
+        answer: "Tu peux toujours réver !",
+    },
+    {
+        id: 6,
+        answer: "Peut-être quand les poules auront des dents...",
+    },
+    {
+        id: 7,
+        answer: "Evidemment !",
+    },
+    {
+        id: 8,
+        answer: "Le monde n'attend que ça",
+    },
+    {
+        id: 9,
+        answer: "Oui !",
+    },
+    {
+        id: 10,
+        answer: "Non !",
+    },
+    {
+        id: 11,
+        answer: "Je ne sais pas !",
+    },
+    {
+        id: 12,
+        answer: "Va voir le chat GPT",
+    },
+    {
+        id: 13,
+        answer: "Pourquoi tu me demandes ça à moi ?",
+    },
+    {
+        id: 14,
+        answer: "Łukasz à ta réponse",
+    },
+];
+
+export default answers
+
